05139a4fc782d99c2ffdfeff10c263b65725c161
omeryildizce <omeryildizce@gmail.com>
2023-04-10 22:48:07 +0300

    Create Country.kt

---
+++ s02_CountriesApp/Countries/app/src/main/java/com/omeryildizce/countries/model/Country.kt
@@ -0,0 +1,5 @@
+package com.omeryildizce.countries.model
+
+
+data class Country(val name:String, val capital:String, val region:String, val currency:String, val language:String, val imageUrl:String)
+
