407f3c816da4b3169b89a8fad0643ff03df91401
omeryildizce <omeryildizce@gmail.com>
2023-04-10 22:48:09 +0300

    Update CountryFragment.kt

--- s02_CountriesApp/Countries/app/src/main/java/com/omeryildizce/countries/view/CountryFragment.kt
+++ s02_CountriesApp/Countries/app/src/main/java/com/omeryildizce/countries/view/CountryFragment.kt
@@ -5,13 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.omeryildizce.countries.view.CountryFragmentArgs
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.get
 import com.example.countries.databinding.FragmentCountryBinding
+import com.omeryildizce.countries.viewmodel.CountryViewModel
 
 
 class CountryFragment : Fragment() {
     private lateinit var binding: FragmentCountryBinding
     private var countryUuid = 0
+    private lateinit var viewModel :CountryViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -27,11 +31,28 @@ class CountryFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        viewModel = ViewModelProvider(this).get(CountryViewModel::class.java)
+        viewModel.getDataRoom()
 
         arguments?.let { bundle ->
             countryUuid = CountryFragmentArgs.fromBundle(bundle).countryUuid
 
         }
+        observeData()
     }
 
+
+    private fun observeData(){
+        viewModel.countryLiveData.observe(viewLifecycleOwner, Observer {
+            country ->
+            country?.let {
+                binding.countryNameTextView.text = it.name
+                binding.capitalTextView.text = it.capital
+                binding.regionTextView.text = it.region
+                binding.currencyTextView.text = it.currency
+                binding.languageTextView.text = it.language
+
+            }
+        })
+    }
 }
