c28837d3c5e7d34e6e46888ee3ede8f03e01669f
omeryildizce <omeryildizce@gmail.com>
2023-04-11 02:13:47 +0300

    delete app

--- s02_CountriesApp/Countries/app/src/main/java/com/omeryildizce/countries/model/Country.kt
+++
@@ -1,5 +0,0 @@
-package com.omeryildizce.countries.model
-
-
-data class Country(val name:String, val capital:String, val region:String, val currency:String, val language:String, val imageUrl:String)
-
