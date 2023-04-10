6094487f508b37c5e545841bf182a6ddc0f7d8ec
omeryildizce <omeryildizce@gmail.com>
2023-04-10 13:37:59 +0300

    Country app  oluştruldu

---
+++ s02_CountriesApp/Countries/app/src/main/java/com/omeryildizce/countries/view/FeedFragment.kt
@@ -0,0 +1,34 @@
+package com.omeryildizce.countries.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import com.omeryildizce.countries.view.FeedFragmentDirections
+import com.example.countries.databinding.FragmentFeedBinding
+
+
+class FeedFragment : Fragment() {
+    private lateinit var binding : FragmentFeedBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentFeedBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+}
