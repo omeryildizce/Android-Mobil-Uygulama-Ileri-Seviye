ef69a00db920647405f527c3239f440baca799ac
omeryildizce <omeryildizce@gmail.com>
2023-04-11 17:49:58 +0300

    Create Util.kt

---
+++ s02_CountriesApp/CountriesApp/app/src/main/java/com/omeryildizce/myapplication/util/Util.kt
@@ -0,0 +1,30 @@
+package com.omeryildizce.myapplication.util
+
+import android.content.Context
+import android.widget.ImageView
+import androidx.swiperefreshlayout.widget.CircularProgressDrawable
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.omeryildizce.myapplication.R
+
+// Extension
+
+fun ImageView.downloadFromUrl(url:String?, progressDrawable: CircularProgressDrawable){
+    val options = RequestOptions().placeholder(progressDrawable)
+        .error(R.drawable.ic_launcher_foreground)
+
+    Glide.with(context)
+        .setDefaultRequestOptions(options)
+        .load(url)
+        .into(this)
+
+
+}
+
+fun  placeHolderProgressBar(context: Context) : CircularProgressDrawable {
+    return CircularProgressDrawable(context).apply {
+        strokeWidth = 8f
+        centerRadius = 40f
+        start()
+    }
+}
