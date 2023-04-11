627e4d8eccd1f5c108295270c711eac8ec6b9603
omeryildizce <omeryildizce@gmail.com>
2023-04-11 23:48:59 +0300

    Update Country.kt

--- s02_CountriesApp/CountriesApp/app/src/main/java/com/omeryildizce/myapplication/model/Country.kt
+++ s02_CountriesApp/CountriesApp/app/src/main/java/com/omeryildizce/myapplication/model/Country.kt
@@ -1,14 +1,32 @@
 package com.omeryildizce.myapplication.model
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
 
-
+@Entity
 data class Country(
+    @ColumnInfo("name")
     @SerializedName("name") val name: String,
+
+    @ColumnInfo("capital")
     @SerializedName("capital") val capital: String,
+
+    @ColumnInfo("region")
     @SerializedName("region") val region: String,
+
+    @ColumnInfo("currency")
     @SerializedName("currency") val currency: String,
+
+    @ColumnInfo("language")
     @SerializedName("language") val language: String,
+
+    @ColumnInfo("flag")
     @SerializedName("flag") val imageUrl: String
-)
+
+){
+    @PrimaryKey(autoGenerate = true)
+    var uuid:Int = 0
+}
 
