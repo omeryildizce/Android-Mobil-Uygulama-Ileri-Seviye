2f0cbfc8ebfd82fc3734d564bf591092ae067a2a
omeryildizce <omeryildizce@gmail.com>
2023-04-11 23:49:13 +0300

    Create CountryDatabase.kt

---
+++ s02_CountriesApp/CountriesApp/app/src/main/java/com/omeryildizce/myapplication/service/CountryDatabase.kt
@@ -0,0 +1,31 @@
+package com.omeryildizce.myapplication.service
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.omeryildizce.myapplication.model.Country
+
+@Database(entities = arrayOf(Country::class), version = 1)
+abstract class CountryDatabase : RoomDatabase(){
+    abstract fun countryDao(): CountryDao
+    // Singelton
+
+    companion object {
+        @Volatile
+        private var instance: CountryDatabase? = null
+        private val lock = Any()
+        operator fun invoke(context: Context) = instance ?: synchronized(lock) {
+            instance ?: makeDatabase(context).also {
+                instance = it
+            }
+        }
+
+        private fun makeDatabase(context: Context) = Room.databaseBuilder(
+            context.applicationContext,
+            CountryDatabase::class.java,
+            "countrydatabase"
+        ).build()
+
+    }
+}
