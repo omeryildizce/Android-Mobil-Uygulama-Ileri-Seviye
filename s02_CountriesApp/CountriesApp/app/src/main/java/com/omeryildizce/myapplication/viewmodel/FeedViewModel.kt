4ba2b86ca9148c0e59b25aa9e7bc6103a08b4d64
omeryildizce <omeryildizce@gmail.com>
2023-04-11 23:49:23 +0300

    Update FeedViewModel.kt

--- s02_CountriesApp/CountriesApp/app/src/main/java/com/omeryildizce/myapplication/viewmodel/FeedViewModel.kt
+++ s02_CountriesApp/CountriesApp/app/src/main/java/com/omeryildizce/myapplication/viewmodel/FeedViewModel.kt
@@ -1,9 +1,12 @@
 package com.omeryildizce.myapplication.viewmodel
 
+import android.app.Application
+import android.widget.Toast
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
 import com.omeryildizce.myapplication.model.Country
 import com.omeryildizce.myapplication.service.CountryAPIService
+import com.omeryildizce.myapplication.service.CountryDatabase
+import com.omeryildizce.myapplication.util.CustomSharedPrefeferences
 
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
@@ -11,30 +14,48 @@ import io.reactivex.observers.DisposableSingleObserver
 
 
 import io.reactivex.schedulers.Schedulers
+import kotlinx.coroutines.launch
 
-
-class FeedViewModel : ViewModel() {
+class FeedViewModel(application: Application) : BaseViewModel(application) {
     private val countryApiService = CountryAPIService()
     private val disposable = CompositeDisposable()
+    private var customPreferences = CustomSharedPrefeferences(getApplication())
+    //              10 minute mm : ss :
+    private var refreshTime = 10 * 60 * 1000 * 1000 * 1000L
+
     val countries = MutableLiveData<List<Country>>()
     val countryError = MutableLiveData<Boolean>()
     val countryLoading = MutableLiveData<Boolean>()
 
-    fun refreshData(){
-        getDataFromAPI()
+    fun refreshData() {
+        val updateTime = customPreferences.getTime()
+        if (updateTime != null && updateTime != 0L && System.nanoTime() - updateTime < refreshTime) {
+            getDataFromSQLite()
+        } else {
+            getDataFromAPI()
+        }
+
     }
 
-    private fun getDataFromAPI(){
+    private fun getDataFromSQLite() {
+        launch {
+            val countries = CountryDatabase(getApplication()).countryDao().getAllCountries()
+            showCountries(countries)
+            Toast.makeText(getApplication(), "Veriler Veritabanından geldi", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun getDataFromAPI() {
         countryLoading.value = true
         disposable.add(
             countryApiService.getData()
                 .subscribeOn(Schedulers.newThread())
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribeWith(object : DisposableSingleObserver<List<Country>>(){
+                .subscribeWith(object : DisposableSingleObserver<List<Country>>() {
                     override fun onSuccess(t: List<Country>) {
-                        countries.value = t
-                        countryError.value = false
-                        countryLoading.value = false
+
+                        stroreInSQLite(t)
+
                     }
 
                     override fun onError(e: Throwable) {
@@ -45,6 +66,31 @@ class FeedViewModel : ViewModel() {
 
                 })
         )
+
+    }
+
+    private fun showCountries(t: List<Country>) {
+        countries.value = t
+        countryError.value = false
+        countryLoading.value = false
+    }
+
+    private fun stroreInSQLite(list: List<Country>) {
+        launch {
+            val dao = CountryDatabase(getApplication()).countryDao()
+            dao.deleteAllCountries()
+            val listLong = dao.insertAll(*list.toTypedArray()) // list -> individual
+            var i = 0
+            while (i < list.size) {
+                list[i].uuid = listLong[i].toInt()
+                i += 1
+            }
+            showCountries(list)
+        }
+        Toast.makeText(getApplication(), "Veriler İnternetten geldi", Toast.LENGTH_LONG).show()
+
+        customPreferences.saveTime(System.nanoTime())
+
 
     }
 }
