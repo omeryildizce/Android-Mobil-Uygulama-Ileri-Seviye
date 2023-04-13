6b4c6681cbf75aab70259bba35dca1cca7267865
omeryildizce <omeryildizce@gmail.com>
2023-04-13 19:05:23 +0300

    Update MainActivity.kt

--- s03_DaggerHilt/HiltKotlin/app/src/main/java/com/omeryildizce/hiltkotlin/MainActivity.kt
+++ s03_DaggerHilt/HiltKotlin/app/src/main/java/com/omeryildizce/hiltkotlin/MainActivity.kt
@@ -3,10 +3,26 @@ package com.omeryildizce.hiltkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    // field injection
+    @Inject
+    lateinit var lars: Musicians
+
+    @Inject
+    lateinit var myClass : ClassExample
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        lars.sing()
+        println( myClass.myFunction())
+        println(myClass.seconfFunction())
     }
 }
+
