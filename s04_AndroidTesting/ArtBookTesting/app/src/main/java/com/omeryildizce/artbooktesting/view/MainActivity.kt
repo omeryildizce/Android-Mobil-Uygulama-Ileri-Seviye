fec27131481d54df3166e2aef04fe7e641194556
omeryildizce <omeryildizce@gmail.com>
2023-04-15 00:54:26 +0300

    Create MainActivity.kt

---
+++ s04_AndroidTesting/ArtBookTesting/app/src/main/java/com/omeryildizce/artbooktesting/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.omeryildizce.artbooktesting.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.omeryildizce.artbooktesting.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
