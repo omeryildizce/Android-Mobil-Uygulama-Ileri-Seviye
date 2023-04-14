6a6415208fc92369ca7e7b9dd5500236c5f0c80b
omeryildizce <omeryildizce@gmail.com>
2023-04-15 00:53:50 +0300

    Create ImageApiFragment.kt

---
+++ s04_AndroidTesting/ArtBookTesting/app/src/main/java/com/omeryildizce/artbooktesting/view/ImageApiFragment.kt
@@ -0,0 +1,12 @@
+package com.omeryildizce.artbooktesting.view
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.omeryildizce.artbooktesting.R
+
+class ImageApiFragment : Fragment(R.layout.fragment_image_api) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
