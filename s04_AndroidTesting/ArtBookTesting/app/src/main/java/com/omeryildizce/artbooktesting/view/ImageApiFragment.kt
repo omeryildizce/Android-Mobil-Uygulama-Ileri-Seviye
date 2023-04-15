6d156dbb8e0b19ec23757e06c89b6411cbe24703
omeryildizce <omeryildizce@gmail.com>
2023-04-15 16:53:54 +0300

    Update ImageApiFragment.kt

--- s04_AndroidTesting/ArtBookTesting/app/src/main/java/com/omeryildizce/artbooktesting/view/ImageApiFragment.kt
+++ s04_AndroidTesting/ArtBookTesting/app/src/main/java/com/omeryildizce/artbooktesting/view/ImageApiFragment.kt
@@ -2,11 +2,77 @@ package com.omeryildizce.artbooktesting.view
 
 import android.os.Bundle
 import android.view.View
+import android.widget.GridLayout
+import android.widget.Toast
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.GridLayoutManager
 import com.omeryildizce.artbooktesting.R
+import com.omeryildizce.artbooktesting.adapter.ImageRecyclerAdapter
+import com.omeryildizce.artbooktesting.databinding.FragmentImageApiBinding
+import com.omeryildizce.artbooktesting.util.Status
+import com.omeryildizce.artbooktesting.viewmodel.ArtViewModel
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class ImageApiFragment : Fragment(R.layout.fragment_image_api) {
+class ImageApiFragment @Inject constructor(private val imageRecyclerAdapter: ImageRecyclerAdapter) :
+    Fragment(R.layout.fragment_image_api) {
+    lateinit var viewModel: ArtViewModel
+    private lateinit var binding: FragmentImageApiBinding
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        viewModel = ViewModelProvider(requireActivity()).get(ArtViewModel::class.java)
+        binding = FragmentImageApiBinding.bind(view)
+        var job: Job? = null
+        binding.searchText.addTextChangedListener{
+            job?.cancel()
+            job = lifecycleScope.launch {
+                delay(1000)
+                it?.let {
+                    if (it.toString().isNotEmpty()){
+                        viewModel.searchforImage(it.toString())
+                    }
+                }
+            }
+        }
+        subscribeToObservers()
+        binding.imageRecyclerView.adapter = imageRecyclerAdapter
+        binding.imageRecyclerView.layoutManager = GridLayoutManager(requireContext(), 3)
+        imageRecyclerAdapter.setOnItemClickListener {
+            findNavController().popBackStack()
+            viewModel.setSelectedImage(it)
+        }
+    }
+
+    fun subscribeToObservers() {
+        viewModel.imageList.observe(viewLifecycleOwner, Observer {
+            when (it.status) {
+                Status.SUCCESS -> {
+                    val urls = it.data?.hits?.map { imageResult ->
+                        imageResult.previewURL
+
+                    }
+                    imageRecyclerAdapter.images = urls ?: listOf()
+                    binding.progressBar.visibility = View.GONE
+                }
+
+                Status.ERROR -> {
+                    Toast.makeText(requireContext(), it.message ?:"Error", Toast.LENGTH_SHORT).show()
+                    binding.progressBar.visibility = View.GONE
+                }
+
+                Status.LOADING -> {
+                    binding.progressBar.visibility = View.VISIBLE
+                }
+            }
+
+        })
     }
 }
