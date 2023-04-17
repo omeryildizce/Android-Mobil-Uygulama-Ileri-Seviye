23127b92dca79cbe0d7e7e28eed40235b1f91a52
omeryildizce <omeryildizce@gmail.com>
2023-04-18 02:36:20 +0300

    Update MainActivity.kt

--- s05_CryptoCrazyApp/CryptoCrazy/app/src/main/java/com/omeryildizce/cryptocrazy/MainActivity.kt
+++ s05_CryptoCrazyApp/CryptoCrazy/app/src/main/java/com/omeryildizce/cryptocrazy/MainActivity.kt
@@ -1,48 +1,60 @@
 package com.omeryildizce.cryptocrazy
 
+
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.omeryildizce.cryptocrazy.ui.theme.CryptoCrazyTheme
+import com.omeryildizce.cryptocrazy.view.CryptoDetailsScreen
+import com.omeryildizce.cryptocrazy.view.CryptoListScreen
+import dagger.hilt.android.AndroidEntryPoint
 
+
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             CryptoCrazyTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+                val navController = rememberNavController()
+                NavHost(navController = navController, startDestination = "crypto_list_screen") {
+                    composable("crypto_list_screen") {
+                        // CryptoListScreen
+                        CryptoListScreen(navController)
+                    }
+
+                    composable("crypto_detail_screen/{cryptoId}/{cryptoPrice}", arguments = listOf(
+                        navArgument("cryptoId") {
+                            type = NavType.StringType
+                        },
+                        navArgument("cryptoPrice") {
+                            type = NavType.StringType
+                        }
+                    )) {
+                        // CryptoDetailsScreen
+                        val cryptoId = remember {
+                            it.arguments?.getString("cryptoId")
+                        }
+                        val cryptoPrice = remember {
+                            it.arguments?.getString("cryptoPrice")
+                        }
+                        CryptoDetailsScreen(cryptoId ?: "", cryptoPrice ?: "", navController)
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "hlalo $name!",
-        modifier = modifier.background(Color.Green)
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CryptoCrazyTheme {
-        Greeting("Android")
     }
 }
