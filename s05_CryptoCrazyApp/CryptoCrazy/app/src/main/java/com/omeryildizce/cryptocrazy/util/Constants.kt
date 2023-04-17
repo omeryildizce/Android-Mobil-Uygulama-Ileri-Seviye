fe6c09d6a9486555d8cccd44f7ef7f2e6917fb60
omeryildizce <omeryildizce@gmail.com>
2023-04-18 02:36:42 +0300

    Create Constants.kt

---
+++ s05_CryptoCrazyApp/CryptoCrazy/app/src/main/java/com/omeryildizce/cryptocrazy/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.omeryildizce.cryptocrazy.util
+
+object Constants {
+    // https://api.coingecko.com/api/v3/coins/list?include_platform=false
+    // https://api.coingecko.com/api/v3/coins/{coin_id}?localization=false&tickers=false&market_data=false&community_data=false&developer_data=false&sparkline=false
+    const val BASE_URL = "https://api.coingecko.com/api/v3/"
+
+}
