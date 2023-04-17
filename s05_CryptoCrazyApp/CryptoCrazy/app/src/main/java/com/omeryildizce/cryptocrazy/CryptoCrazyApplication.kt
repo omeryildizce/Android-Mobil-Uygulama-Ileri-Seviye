2c16391669358a2520bd353c8c2c3d2dd72f6f77
omeryildizce <omeryildizce@gmail.com>
2023-04-18 02:36:14 +0300

    Create CryptoCrazyApplication.kt

---
+++ s05_CryptoCrazyApp/CryptoCrazy/app/src/main/java/com/omeryildizce/cryptocrazy/CryptoCrazyApplication.kt
@@ -0,0 +1,9 @@
+package com.omeryildizce.cryptocrazy
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class CryptoCrazyApplication : Application() {
+
+}
