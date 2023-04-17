61239db4e05fe7bd4bec52ada282060888f35af4
omeryildizce <omeryildizce@gmail.com>
2023-04-18 02:36:17 +0300

    Create AppModule.kt

---
+++ s05_CryptoCrazyApp/CryptoCrazy/app/src/main/java/com/omeryildizce/cryptocrazy/dependencyinjection/AppModule.kt
@@ -0,0 +1,33 @@
+package com.omeryildizce.cryptocrazy.dependencyinjection
+
+import com.omeryildizce.cryptocrazy.repository.CryptoRepository
+import com.omeryildizce.cryptocrazy.service.CryptoAPI
+import com.omeryildizce.cryptocrazy.util.Constants.BASE_URL
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Singleton
+    @Provides
+    fun provideCryptoRepository(
+        api: CryptoAPI
+    ) = CryptoRepository(api)
+
+    @Singleton
+    @Provides
+    fun provideCryptoApi(): CryptoAPI {
+        return Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl(BASE_URL)
+            .build()
+            .create(CryptoAPI::class.java)
+    }
+}
