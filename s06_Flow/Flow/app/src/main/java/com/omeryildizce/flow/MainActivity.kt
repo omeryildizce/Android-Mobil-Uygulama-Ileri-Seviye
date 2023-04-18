a2261b9accc3fd17af442829a0823f93bb540a19
omeryildizce <omeryildizce@gmail.com>
2023-04-18 20:19:29 +0300

    Update MainActivity.kt

--- s06_Flow/Flow/app/src/main/java/com/omeryildizce/flow/MainActivity.kt
+++ s06_Flow/Flow/app/src/main/java/com/omeryildizce/flow/MainActivity.kt
@@ -3,13 +3,20 @@ package com.omeryildizce.flow
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.State
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.omeryildizce.flow.ui.theme.FlowTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,30 +24,26 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             FlowTheme {
-                // A surface container using the 'background' color from the theme
+                val viewModel: MyViewModel by viewModels()
+                val counter = viewModel.coutDownTimerFlow.collectAsState(initial = 10)
+
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Box(modifier = Modifier.fillMaxSize()) {
+                        Text(
+                            text = counter.value.toString(),
+                            fontSize = 50.sp,
+                            textAlign = TextAlign.Center,
+                        modifier = Modifier.align(Alignment.Center)
+                        )
+
+                    }
                 }
             }
         }
     }
+
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    FlowTheme {
-        Greeting("Android")
-    }
-}
