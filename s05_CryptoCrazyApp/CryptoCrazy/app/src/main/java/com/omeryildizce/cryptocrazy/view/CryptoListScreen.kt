f517a6ee3ac3ff8bb91669a902bb638e3d6b76f3
omeryildizce <omeryildizce@gmail.com>
2023-04-18 18:02:10 +0300

    Update CryptoListScreen.kt

--- s05_CryptoCrazyApp/CryptoCrazy/app/src/main/java/com/omeryildizce/cryptocrazy/view/CryptoListScreen.kt
+++ s05_CryptoCrazyApp/CryptoCrazy/app/src/main/java/com/omeryildizce/cryptocrazy/view/CryptoListScreen.kt
@@ -1,26 +1,245 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package com.omeryildizce.cryptocrazy.view
 
 
 import android.content.res.Resources.Theme
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Clear
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldColors
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import com.omeryildizce.cryptocrazy.model.CryptoListItem
 import com.omeryildizce.cryptocrazy.viewmodel.CryptoListViewModel
 
 
 @Composable
 fun CryptoListScreen(
-    navController: NavController ,
+    navController: NavController,
     viewModel: CryptoListViewModel = hiltViewModel()
 ) {
-    Surface(color = MaterialTheme.colorScheme.secondary , modifier = Modifier.fillMaxSize()) {
-        
+    Surface(color = MaterialTheme.colorScheme.secondary, modifier = Modifier.fillMaxSize()) {
+        Column(modifier = Modifier.fillMaxSize()) {
+            Text(
+                text = "Crypto Crazy",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(20.dp),
+                textAlign = TextAlign.Center,
+                fontSize = 40.sp,
+                fontWeight = FontWeight.Bold,
+                color = MaterialTheme.colorScheme.primary
+            )
+
+            Spacer(modifier = Modifier.height(10.dp))
+            SearchBar(Modifier.padding(20.dp), "Search ...") {
+                viewModel.searchCryptoList(it)
+            }
+            Spacer(modifier = Modifier.height(10.dp))
+            CryptoList(navController)
+
+        }
+    }
+}
+
+
+@Composable
+fun SearchBar(
+    modifier: Modifier,
+    hint: String = "",
+    onSearch: (String) -> Unit = {}
+) {
+    var text = remember { mutableStateOf("") }
+
+    TextField(
+        value = text.value,
+        onValueChange = {
+            text.value = it
+            onSearch(it)
+        },
+        modifier = modifier
+
+            .fillMaxWidth()
+            .shadow(5.dp, RoundedCornerShape(8.dp)),
+        singleLine = true,
+        colors = TextFieldDefaults.textFieldColors(
+
+            textColor = MaterialTheme.colorScheme.primary,
+            cursorColor = MaterialTheme.colorScheme.primary,
+            focusedIndicatorColor = Color.Transparent,
+            unfocusedIndicatorColor = Color.Transparent,
+            disabledIndicatorColor = Color.Transparent,
+            containerColor = MaterialTheme.colorScheme.secondary
+        ),
+        placeholder = {
+            Text(
+                text = hint,
+                color = MaterialTheme.colorScheme.primary.copy(alpha = 0.5f)
+            )
+        },
+        leadingIcon = {
+            Icon(
+                imageVector = Icons.Default.Search,
+                contentDescription = "Search Icon",
+                tint = MaterialTheme.colorScheme.primary.copy(alpha = 0.5f)
+            )
+        },
+        trailingIcon = {
+            if (text.value.isNotEmpty()) {
+                IconButton(
+                    onClick = { text.value = "" }
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.Clear,
+                        contentDescription = "Clear Text",
+                        tint = MaterialTheme.colorScheme.primary.copy(alpha = 0.5f)
+                    )
+                }
+            }
+        }
+    )
+
+}
+
+@Composable
+fun CryptoList(navController: NavController, viewModel: CryptoListViewModel = hiltViewModel()) {
+    val cryptoList by remember {
+        viewModel.cryptoList
+    }
+    val errorMessage by remember {
+        viewModel.errorMessage
+    }
+    val isLoading by remember {
+        viewModel.isLoading
+    }
+
+    CryptoListView(cryptos = cryptoList, navController = navController)
+    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+        if (isLoading) {
+            CircularProgressIndicator(color = MaterialTheme.colorScheme.primary)
+
+        }
+        if (errorMessage.isNotEmpty()) {
+            RetryView(error = errorMessage) {
+                viewModel.loadCryptos()
+            }
+        }
+    }
+}
+
+@Composable
+fun CryptoListView(cryptos: List<CryptoListItem>, navController: NavController) {
+    LazyColumn {
+        items(cryptos) { crypto ->
+            CryptoRow(navController = navController, crypto = crypto)
+
+        }
+    }
+
+}
+
+@Composable
+fun CryptoRow(navController: NavController, crypto: CryptoListItem) {
+    Card(elevation = CardDefaults.cardElevation(8.dp), modifier = Modifier.padding(16.dp)) {
+
+        Column(
+            modifier = Modifier
+
+                .fillMaxWidth()
+                .background(MaterialTheme.colorScheme.secondary)
+                .clickable {
+                    navController.navigate("crypto_detail_screen/${crypto.id}/${crypto.name}")
+                }
+        ) {
+
+            Text(
+                text = crypto.name,
+                color = MaterialTheme.colorScheme.primary,
+                style = MaterialTheme.typography.headlineMedium,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(vertical = 4.dp, horizontal = 16.dp)
+            )
+            Text(
+                text = crypto.symbol,
+                color = MaterialTheme.colorScheme.primary,
+                style = MaterialTheme.typography.bodyMedium,
+                modifier = Modifier.padding(vertical = 4.dp, horizontal = 16.dp)
+
+            )
+
+
+        }
+    }
+
+
+}
+
+@Composable
+fun RetryView(
+    error: String,
+    onRetry: () -> Unit
+) {
+    Card(elevation = CardDefaults.cardElevation(8.dp) , colors = CardDefaults.cardColors(MaterialTheme.colorScheme.secondary) ) {
+
+        Column(modifier = Modifier.fillMaxSize(0.5f), horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.SpaceEvenly) {
+            Text(text = error, fontSize = 20.sp, color = MaterialTheme.colorScheme.error)
+            Spacer(modifier = Modifier.height(10.dp))
+            Button(
+                modifier = Modifier.padding(horizontal = 20.dp).fillMaxWidth(),
+                onClick = onRetry
+
+            ) {
+                Text(text = "Retry", color = MaterialTheme.colorScheme.secondary)
+            }
+        }
     }
 }
