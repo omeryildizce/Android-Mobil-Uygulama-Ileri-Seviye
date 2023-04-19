f977b5d3899b105bf5b8a0ffc9c7a4892537acc9
omeryildizce <omeryildizce@gmail.com>
2023-04-19 20:36:01 +0300

    Create MainActivity.kt

---
+++ s07_CryptoCrazy/app/src/main/java/com/omeryildizce/cryptocrazy/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.omeryildizce.cryptocrazy.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.omeryildizce.cryptocrazy.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
