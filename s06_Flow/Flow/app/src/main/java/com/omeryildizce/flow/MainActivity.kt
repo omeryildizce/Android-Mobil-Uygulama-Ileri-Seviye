f85bff1c6aacd1864a9753f823926b07cd7a9967
omeryildizce <omeryildizce@gmail.com>
2023-04-19 12:34:58 +0300

    Update MainActivity.kt

--- s06_Flow/Flow/app/src/main/java/com/omeryildizce/flow/MainActivity.kt
+++ s06_Flow/Flow/app/src/main/java/com/omeryildizce/flow/MainActivity.kt
@@ -4,20 +4,26 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.State
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.omeryildizce.flow.ui.theme.FlowTheme
+import kotlinx.coroutines.flow.StateFlow
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,21 +31,13 @@ class MainActivity : ComponentActivity() {
         setContent {
             FlowTheme {
                 val viewModel: MyViewModel by viewModels()
-                val counter = viewModel.coutDownTimerFlow.collectAsState(initial = 10)
 
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Box(modifier = Modifier.fillMaxSize()) {
-                        Text(
-                            text = counter.value.toString(),
-                            fontSize = 50.sp,
-                            textAlign = TextAlign.Center,
-                        modifier = Modifier.align(Alignment.Center)
-                        )
-
-                    }
+                    //FirstScreen(viewModel)
+                    SecondScreen(viewModel)
                 }
             }
         }
@@ -47,3 +45,50 @@ class MainActivity : ComponentActivity() {
 
 }
 
+@Composable
+fun SecondScreen(viewModel: MyViewModel) {
+    val liveDataValue = viewModel.liveData.observeAsState()
+    val stateFlowValue = viewModel.stateFlow.collectAsState()
+    val sharedFlowValue = viewModel.sharedFlow.collectAsState(initial = " ")
+
+
+        Column(
+            verticalArrangement = Arrangement.SpaceEvenly,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+
+            Text(text = liveDataValue.value ?: "")
+            Button(onClick = { viewModel.changeLiveDataValue() }) {
+                Text(text = "Live Data Button")
+            }
+            Spacer(modifier = Modifier.padding(10.dp))
+            Text(text = stateFlowValue.value ?: "")
+            Button(onClick = { viewModel.changeStateFlowValue() }) {
+                Text(text = "State Flow Button")
+            }
+            Spacer(modifier = Modifier.padding(10.dp))
+
+            Text(text = sharedFlowValue.value ?: "")
+            Button(onClick = { viewModel.changeSharedFlow() }) {
+                Text(text = "Shared Flow Button")
+            }
+
+
+        }
+
+}
+
+@Composable
+fun FirstScreen(viewModel: MyViewModel) {
+    val counter = viewModel.coutDownTimerFlow.collectAsState(initial = 10)
+    Box(modifier = Modifier.fillMaxSize()) {
+        Text(
+            text = counter.value.toString(),
+            fontSize = 50.sp,
+            textAlign = TextAlign.Center,
+            modifier = Modifier.align(Alignment.Center)
+        )
+
+    }
+
+}
