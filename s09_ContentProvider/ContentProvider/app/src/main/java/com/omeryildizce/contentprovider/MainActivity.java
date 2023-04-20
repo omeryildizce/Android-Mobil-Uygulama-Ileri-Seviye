0769a2b906edddd43ad71f7543e2e4bb12344e11
omeryildizce <omeryildizce@gmail.com>
2023-04-20 17:55:47 +0300

    Update MainActivity.java

--- s09_ContentProvider/ContentProvider/app/src/main/java/com/omeryildizce/contentprovider/MainActivity.java
+++ s09_ContentProvider/ContentProvider/app/src/main/java/com/omeryildizce/contentprovider/MainActivity.java
@@ -1,18 +1,27 @@
 package com.omeryildizce.contentprovider;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.app.Activity;
 import android.content.ContentResolver;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.ContactsContract;
+import android.provider.Settings;
+import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     FloatingActionButton fab;
@@ -22,7 +31,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_CONTACTS}, 1);
+        }
         listView = findViewById(R.id.listView);
 
         fab = findViewById(R.id.floatingActionButton);
@@ -32,8 +43,35 @@ public class MainActivity extends AppCompatActivity {
                 ContentResolver contentResolver = getContentResolver();
                 String[] projection = {ContactsContract.Contacts.DISPLAY_NAME};
                 Cursor cursor = contentResolver.query(ContactsContract.Contacts.CONTENT_URI, projection, null, null, ContactsContract.Contacts.DISPLAY_NAME);
+
+                if (cursor != null) {
+
+                    ArrayList<String> contactList = new ArrayList<>();
+                    int displayNameIndex = cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME);
+                    while (cursor.moveToNext()) {
+                        contactList.add(cursor.getString(displayNameIndex));
+                    }
+                    cursor.close();
+                    ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1, contactList);
+                    listView.setAdapter(adapter);
+                }
             } else {
-                Snackbar.make(view, "Replace with your action", Snackbar.LENGTH_LONG).setAction("Action", null).show();
+                Snackbar.make(view, "Permission needed", Snackbar.LENGTH_LONG).setAction("Action", null)
+                        .setAction("Give Permission", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this, Manifest.permission.READ_CONTACTS)) {
+
+                                    ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.READ_CONTACTS}, 2);
+                                }else {
+                                    Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                                    Uri uri = Uri.fromParts("package", getPackageName(), null);
+                                    intent.setData(uri);
+                                    startActivity(intent);
+                                }
+                            }
+                        })
+                        .show();
             }
         });
     }
