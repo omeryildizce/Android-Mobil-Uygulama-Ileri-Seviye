062ffd7fc32fd2411ef90aa9e65ef392e2ef6c3d
omeryildizce <omeryildizce@gmail.com>
2023-04-20 12:38:52 +0300

    Update MainActivity.kt

--- s08_ActivityCycle/ActivityCycle/app/src/main/java/com/omeryildizce/activitycycle/MainActivity.kt
+++ s08_ActivityCycle/ActivityCycle/app/src/main/java/com/omeryildizce/activitycycle/MainActivity.kt
@@ -1,11 +1,81 @@
 package com.omeryildizce.activitycycle
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.Log
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var editText: EditText;
+    lateinit var textView: TextView;
+    private val TAG = "MainActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d(TAG, "onCreate: ")
+        editText = findViewById(R.id.editTextText)
+        textView = findViewById(R.id.textView)
+
     }
+
+
+    override fun onSaveInstanceState(outState: Bundle, outPersistentState: PersistableBundle) {
+        super.onSaveInstanceState(outState, outPersistentState)
+        Log.d(TAG, "onSaveInstanceState: ")
+        outState.putString("saved", textView.text.toString())
+    }
+
+    override fun onRestoreInstanceState(
+        savedInstanceState: Bundle?,
+        persistentState: PersistableBundle?
+    ) {
+        super.onRestoreInstanceState(savedInstanceState, persistentState)
+        Log.d(TAG, "onRestoreInstanceState: ")
+         var text=   savedInstanceState?.getString("save") ?: "Hello"
+
+        textView.setText(text)
+
+    }
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart: ")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume: ")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause: ")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop: ")
+    }
+
+    override fun onDestroy() {
+        Log.d(TAG, "onDestroy: ")
+        super.onDestroy()
+    }
+    fun getName(view: View) {
+
+        var nameInput: String = editText.text.toString()
+        textView.setText(nameInput)
+
+    }
+
+    fun secondActivity(view: View) {
+        var intent = Intent(this, MainActivity2::class.java)
+        startActivity(intent)
+        finish()
+    }
+
+
 }
