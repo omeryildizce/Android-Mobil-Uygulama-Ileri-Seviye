7b58f9bc2de6702f1ea2bba89c200d510c636302
omeryildizce <omeryildizce@gmail.com>
2023-04-21 19:45:44 +0300

    Create ServiceClass.java

---
+++ s10_Service/Service/app/src/main/java/com/omeryildizce/service/ServiceClass.java
@@ -0,0 +1,36 @@
+package com.omeryildizce.service;
+
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+    
+public class ServiceClass extends Service {
+    private static final String TAG = "ServiceClass";
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.d(TAG, "onCreate: ");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy: ");
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.d(TAG, "onStartCommand: ");
+        return super.onStartCommand(intent, flags, startId);
+    }
+}
