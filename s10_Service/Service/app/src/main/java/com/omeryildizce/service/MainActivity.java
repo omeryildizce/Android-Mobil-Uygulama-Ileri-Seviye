7b0b6044ff98ba4c5e07fd2047efb2f5d8ab352f
omeryildizce <omeryildizce@gmail.com>
2023-04-21 19:45:46 +0300

    Update MainActivity.java

--- s10_Service/Service/app/src/main/java/com/omeryildizce/service/MainActivity.java
+++ s10_Service/Service/app/src/main/java/com/omeryildizce/service/MainActivity.java
@@ -2,13 +2,46 @@ package com.omeryildizce.service;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    Button startService, stopService;
+    EditText editText;
+    Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initalize();
+        intent = new Intent(getApplicationContext(), ServiceClass.class);
+        service();
+    }
+
+    private void service() {
+        startService.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startService(intent);
+            }
+        });
+
+        stopService.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stopService(intent);
+            }
+        });
+    }
+
+    private void initalize() {
+        startService = findViewById(R.id.buttonStart);
+        stopService = findViewById(R.id.buttonStop);
+        editText = findViewById(R.id.editTextText);
     }
 }
