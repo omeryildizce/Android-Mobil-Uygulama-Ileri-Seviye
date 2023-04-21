baa150554a6c0c3e941e28be0ad42594b95d4dd1
omeryildizce <omeryildizce@gmail.com>
2023-04-21 22:17:22 +0300

    Update MainActivity.java

--- s10_Service/ResultReciver/app/src/main/java/com/omeryildizce/resultreciver/MainActivity.java
+++ s10_Service/ResultReciver/app/src/main/java/com/omeryildizce/resultreciver/MainActivity.java
@@ -2,13 +2,60 @@ package com.omeryildizce.resultreciver;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.ResultReceiver;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText editText;
+    TextView textView;
+    Handler handler = new Handler();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editText = findViewById(R.id.enterUrl);
+        textView = findViewById(R.id.textView);
+    }
+
+    public void download(View view) {
+        ResultReceiver myResultReceiver = new MyResultReciver(null);
+
+
+
+        Intent intent = new Intent(this,Download.class);
+        String userInput = editText.getText().toString();
+        intent.putExtra("url", userInput);
+        textView.setText("downloading...");
+        intent.putExtra("receiver",myResultReceiver);
+
+        startService(intent);
+    }
+
+    public class MyResultReciver extends ResultReceiver {
+         public MyResultReciver(Handler handler) {
+            super(handler);
+        }
+
+        @Override
+        protected void onReceiveResult(int resultCode, Bundle resultData) {
+
+            if (resultCode == 1 && resultData != null) {
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        String result = resultData.getString("websiteResult");
+                        textView.setText(result);
+                    }
+                });
+            }
+
+            super.onReceiveResult(resultCode, resultData);
+        }
     }
 }
