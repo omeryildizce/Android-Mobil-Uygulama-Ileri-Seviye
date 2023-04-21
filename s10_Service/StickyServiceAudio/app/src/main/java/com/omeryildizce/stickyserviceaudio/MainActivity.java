8af9ff2ab06847e07ec0f3f8b6dcc95c73dc6928
omeryildizce <omeryildizce@gmail.com>
2023-04-21 22:43:07 +0300

    Update MainActivity.java

--- s10_Service/StickyServiceAudio/app/src/main/java/com/omeryildizce/stickyserviceaudio/MainActivity.java
+++ s10_Service/StickyServiceAudio/app/src/main/java/com/omeryildizce/stickyserviceaudio/MainActivity.java
@@ -2,13 +2,31 @@ package com.omeryildizce.stickyserviceaudio;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    Intent intent;
+
+    MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void play(View view) {
+        intent = new Intent(this, StickService.class);
+        startService(intent);
+    }
+
+    public void stop(View view) {
+        intent = new Intent(this, StickService.class);
+
+        stopService(intent);
     }
 }
