5546d37b7912dd07ab15ef7b025e5845f3e7f0e7
omeryildizce <omeryildizce@gmail.com>
2023-04-22 19:26:35 +0300

    Create MyPhoneReceiver.java

---
+++ s11_BroadcastReciver/PhoneManagerBroadcast/app/src/main/java/com/omeryildizce/phonemanagerbroadcast/MyPhoneReceiver.java
@@ -0,0 +1,33 @@
+package com.omeryildizce.phonemanagerbroadcast;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.telephony.PhoneStateListener;
+import android.telephony.TelephonyManager;
+
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
+
+public class MyPhoneReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(final Context context, Intent intent) {
+
+        TelephonyManager telephonyManager = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+        telephonyManager.listen(new PhoneStateListener() {
+
+            @Override
+            public void onCallStateChanged(int state, String incomingNumber) {
+                super.onCallStateChanged(state, incomingNumber);
+
+
+                System.out.println("incomingNumber: " + incomingNumber);
+
+                LocalBroadcastManager manager = LocalBroadcastManager.getInstance(context);
+                Intent intentToMain = new Intent("my.result.receiver");
+                intentToMain.putExtra("incomingNumber",incomingNumber);
+                manager.sendBroadcast(intentToMain);
+
+            }
+        },PhoneStateListener.LISTEN_CALL_STATE);
+    }
+}
