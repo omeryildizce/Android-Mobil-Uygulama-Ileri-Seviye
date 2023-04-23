8a566e21fd68899ebbb542ee3140caf64b01e564
omeryildizce <omeryildizce@gmail.com>
2023-04-23 12:06:30 +0300

    Create PopupFragment.java

---
+++ s12_FitnessApp/FitnessApp/app/src/main/java/com/omeryildizce/fitnessapp/fragment/PopupFragment.java
@@ -0,0 +1,75 @@
+package com.omeryildizce.fitnessapp.fragment;
+
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.omeryildizce.fitnessapp.FitnessMove;
+import com.omeryildizce.fitnessapp.R;
+import com.squareup.picasso.Callback;
+import com.squareup.picasso.Picasso;
+
+
+public class PopupFragment extends Fragment {
+
+    TextView nameText;
+    TextView descriptionText;
+    TextView calorieText;
+    ImageView imageView;
+    ProgressBar progressBar;
+    private FitnessMove fitnessMove;
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.fragment_popup,container,false);
+        nameText = rootView.findViewById(R.id.fragment_popup_name_textview);
+        descriptionText = rootView.findViewById(R.id.fragment_popup_description_textview);
+        calorieText = rootView.findViewById(R.id.fragment_popup_calorie_textview);
+        imageView = rootView.findViewById(R.id.fragment_popup_imageview);
+        progressBar = rootView.findViewById(R.id.fragment_popup_progressbar);
+
+        nameText.setText(fitnessMove.getFitnessName());
+        descriptionText.setText(fitnessMove.getFitnessDescription());
+        calorieText.setText(""+fitnessMove.getFitnessCalorie());
+
+        Picasso.get().load(fitnessMove.getFitnessPicture()).fit().centerCrop().into(imageView, new Callback() {
+            @Override
+            public void onSuccess() {
+                progressBar.setVisibility(View.INVISIBLE);
+            }
+
+            @Override
+            public void onError(Exception e) {
+
+            }
+        });
+
+
+        return rootView;
+    }
+
+    public static PopupFragment newInstance() {
+        return new PopupFragment();
+    }
+
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        fitnessMove = getActivity().getIntent().getParcelableExtra("INFO");
+
+    }
+}
